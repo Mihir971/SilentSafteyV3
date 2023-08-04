ee752e8f9719c5e09baa7cb3ede3b2ccd56741d4
Mihir <mihirthak9971@gmail,com>
2023-08-04 14:36:36 -0500

    Initial Commit

---
+++ SilentSafteyV3/SilentSafteyV3App.swift
@@ -0,0 +1,17 @@
+//
+//  SilentSafteyV3App.swift
+//  SilentSafteyV3
+//
+//  Created by Mihir Thakur on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SilentSafteyV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
